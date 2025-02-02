9061a0179fd69c2832b4d57612a4a97421cad1c6
Junkai Man <60953254+junkaiman@users.noreply.github.com>
2025-02-02 10:15:52 -0500

    change logo look

--- app/src/components/TopHeader.tsx
+++ app/src/components/TopHeader.tsx
@@ -1,10 +1,8 @@
 import React from "react";
-import Logo from "./Logo";
 
 const Header: React.FC = () => {
   return (
     <div className="bg-[#7252FE] w-full p-10 text-white rounded-bl-3xl rounded-br-3xl h-[25vh]">
-      <Logo />
       <div className="text-md mb-3">{"Welcome Back!"}</div>
       <div className="text-3xl">{"Let's find your medication."}</div>
     </div>
